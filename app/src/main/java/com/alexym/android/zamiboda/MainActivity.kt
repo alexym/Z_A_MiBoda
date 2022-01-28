195647a72df78a725ab9db129bafcea01e018de0
Alex <a.rodarte@alphasoluciones.com>
2022-01-28 17:14:33 -0600

    se implementa invitacion principal con navegacion

--- app/src/main/java/com/alexym/android/zamiboda/MainActivity.kt
+++ app/src/main/java/com/alexym/android/zamiboda/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.alexym.android.zamiboda
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
 import com.alexym.android.zamiboda.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,16 +29,22 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.appBarMain.toolbar)
 
         binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            val gmmIntentUri =
+                Uri.parse("google.navigation:q=Thesan+Jardín+Temixco+Morelos")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
         }
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_location), drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_location
+            ), drawerLayout
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
--- app/src/main/java/com/alexym/android/zamiboda/ui/home/HomeFragment.kt
+++ app/src/main/java/com/alexym/android/zamiboda/ui/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.alexym.android.zamiboda.ui.home
 
+import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +10,14 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.alexym.android.zamiboda.R
+import com.alexym.android.zamiboda.databinding.FragmentHomeBBinding
 import com.alexym.android.zamiboda.databinding.FragmentHomeBinding
 import com.alexym.android.zamiboda.utils.konfetti.Presets.Companion.explode
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions.bitmapTransform
+import jp.wasabeef.blurry.Blurry
+import jp.wasabeef.glide.transformations.BlurTransformation
 import nl.dionsegijn.konfetti.core.Party
 import nl.dionsegijn.konfetti.xml.KonfettiView
 import nl.dionsegijn.konfetti.xml.listeners.OnParticleSystemUpdateListener
@@ -18,7 +26,7 @@ import nl.dionsegijn.konfetti.xml.listeners.OnParticleSystemUpdateListener
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentHomeBBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -32,7 +40,7 @@ class HomeFragment : Fragment() {
         homeViewModel =
             ViewModelProvider(this)[HomeViewModel::class.java]
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentHomeBBinding.inflate(inflater, container, false)
         initView()
         return binding.root
     }
@@ -51,6 +59,27 @@ class HomeFragment : Fragment() {
         homeViewModel.seconds.observe(viewLifecycleOwner, Observer {
             binding.secondsCountTv.text = it
         })
+        // Sync
+//        val bitmap = Blurry.with(activity)
+//            .radius(10)
+//            .sampling(8)
+//            .capture(findViewById(R.id.right_bottom)).get()
+//        imageView.setImageDrawable(BitmapDrawable(resources, bitmap))
+
+// Async
+//        Blurry.with(activity)
+//            .radius(25)
+//            .sampling(4)
+//            .color(Color.argb(66, 255, 255, 0))
+//            .capture(binding.imageView2)
+//            .getAsync {
+//                binding.imageView2.setImageDrawable(BitmapDrawable(resources, it))
+//            }
+//        context?.let {
+//            Glide.with(it).load(resources.getDrawable(R.drawable.wedding))
+//                .apply(bitmapTransform( BlurTransformation(12)))
+//                .into(binding.imageView2)
+//        };
     }
 
     private fun initKonfetti() {
